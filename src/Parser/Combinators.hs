04b1ef79fd2486187d5aa8b8864ab11788f0f593
orcabreezy <erxc.sabel@gmail.com>
2024-01-30 20:04:03 +0100

    added combinators to handle errors

--- src/Parser/Combinators.hs
+++ src/Parser/Combinators.hs
@@ -1,36 +1,49 @@
 module Parser.Combinators where
 
 import Scanner.Token
-import GHC.Float (showSignedFloat)
 
 type Parser toks a = [toks] -> [(a, [toks])]
 
-data ParseError = UnknownError
-                | PError String
+-- for now an error is just a String
+type ParseError = String
+
+type ParserWithError tok a = [tok] -> Either ParseError (a, [tok]) 
 
 failure :: Parser a b                       -- Parser der leeren Sprache
 failure _ = []
 
-failureWithError :: Parser PositionedToken (Either PositionedToken ParseError)
-failureWithError [] = [(Right UnknownError, [])]
-failureWithError (tkn : tkns) = [(Right (PError ("error on token: " ++ show (token tkn) ++ " at position: " ++ show (position tkn))),tkns)]
- 
-
 succeed :: a -> Parser tok a                -- Parser der Sprache des 
 succeed value toks = [(value, toks)]        -- leeren Worts (\epsilon)
 
-
 -- conditional recognition
-satisfy :: (tok -> Bool) -> Parser tok tok
+satisfy :: (tok -> Bool) -> Parser tok tok 
 satisfy _ [] = []
 satisfy cond (tkn : tkns) | cond tkn  = succeed tkn tkns
                           | otherwise = failure tkns
 
-satisfyWithError :: (PositionedToken -> Bool) -> Parser PositionedToken (Either PositionedToken ParseError)
-satisfyWithError _[] = []
-satisfyWithError cond (tkn : tkns) | cond tkn = succeed (Left tkn) tkns 
-                                   | otherwise = failureWithError (tkn:tkns)
 
+
+-- AIO recognition a singular token with error handling and position tracking
+recon :: PositionedToken -> ParserWithError PositionedToken PositionedToken
+recon tkn [] = Left ("unexpected end of Input, expected: " ++ show (token tkn))
+recon tkn (tok : toks)  | equivalent (token tkn) (token tok) = Right (tok, toks)
+                        | otherwise = Left ("unexpected token: " ++ show tok ++ ", expected: " ++ show (token tkn) 
+                                            ++ "at " ++ show (position tkn))
+            where
+                equivalent (IDENTIFIER _) (IDENTIFIER _) = True
+                equivalent (INTLITERAL _) (INTLITERAL _) = True
+                equivalent (BOOLLITERAL _) (BOOLLITERAL _) = True
+                equivalent (CHARLITERAL _) (CHARLITERAL _) = True
+                equivalent tkn1 tkn2 = tkn1 == tkn2
+
+-- functions to recognize tokens with error handling
+satisfyE :: Show tok =>  (tok -> Bool) -> ParserWithError tok tok
+satisfyE _ [] = Left "unexpected end of Input"
+satisfyE cond (tkn : tkns)  | cond tkn = Right (tkn, tkns)
+                            | otherwise = Left ("unexpected token: " ++ show tkn)
+
+lexemE :: (Eq tok, Show tok) => tok -> ParserWithError tok tok
+lexemE tkn = satisfyE (== tkn)
 
 -- recognition of a lexem (terminal)
 lexem :: Token -> Parser Token Token
@@ -44,6 +57,7 @@ lexemParam tok = satisfy (equivalent tok) where
             equivalent (BOOLLITERAL _) (BOOLLITERAL _) = True
             equivalent (CHARLITERAL _) (CHARLITERAL _) = True
             equivalent tkn1 tkn2 = tkn1 == tkn2
+
 
 --the same functions, just for recognition of tokens, wrapped with their Position
 posLexem :: Token -> Parser PositionedToken PositionedToken
@@ -70,6 +84,28 @@ posLexemParam tkn = satisfy (equivalent tkn . token) where
 (p <<< f) toks = [ (f v, rest) | (v, rest) <- p toks]
 
 infixr +.+, |||
+
+-- combinators, semanitally identicalwith but with error handling support
+(+++) :: ParserWithError tok a -> ParserWithError tok b -> ParserWithError tok (a,b)
+(p1 +++ p2) toks = case p1 toks of
+    Left err -> Left err
+    Right (v1, rest1) -> case p2 rest1 of
+        Left err -> Left err
+        Right (v2, rest2) -> Right ((v1, v2), rest2)
+
+(<||>) :: ParserWithError tok a -> ParserWithError tok a -> ParserWithError tok a
+(p1 <||> p2) toks = case p1 toks of
+    Left err -> case p2 toks of
+        Left err2 -> Left (err ++ " and " ++ err2) -- might wanna changes this to "or" in the future
+        Right (v2, rest2) -> Right (v2, rest2)
+    Right (v1, rest1) -> Right (v1, rest1)
+
+(<$$>) :: ParserWithError tok a -> (a -> b) -> ParserWithError tok b
+(p <$$> f) toks = case p toks of
+    Left err -> Left err
+    Right (v, rest) -> Right (f v, rest)
+infixr +++, <||>
+
 
 -- parse zero or n-many times the thing, p parses, and concat to list
 many :: Parser tok a -> Parser tok [a]
