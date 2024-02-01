b6fb162fef4842b6cfedc4696314e9507b713d1a
p-98 <34243013+p-98@users.noreply.github.com>
2024-02-01 15:31:06 +0100

    main function

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,38 @@
 module Main (main) where
 
-import ByteCodeGen.ByteCodeGen
-import ByteCodeGen.JavaTestFiles.Classes.ClassesTAST
-import ConstantPool.ConstantPool as CP
-import Debug.Trace (traceShowId)
-import Jvm.BinaryClass (encodeClassFile)
-import Types.TAST
+import           ByteCodeGen.ByteCodeGen
+import           ConstantPool.ConstantPool (buildConstantPool)
+import           Control.Monad             (zipWithM_)
+import           Debug.Trace               (traceShowId)
+import           Jvm.BinaryClass           (encodeClassFile)
+import           Parser.IndexParser        (parser)
+import           Scanner.Lexer             (scanner)
+import           SemanticCheck.Typecheck   (typecheck)
+import           Types.TAST                (Class (cname))
+
+examplePath = "Examples/Main.java"
+
+fromRight' :: Either String b -> b
+fromRight' (Left err)   = error err
+fromRight' (Right succ) = succ
+
+compile :: IO ()
+compile = do
+  -- putStrLn "Enter a file to compile: "
+  -- filePath <- getLine
+  let filePath = examplePath
+  file <- readFile filePath
+
+  let lexems = scanner file
+  let ast = fromRight' $ parser lexems
+  let tast = typecheck ast
+  let withPools = toSnd buildConstantPool <$> tast
+  let classFiles = codeGen withPools
+
+  let filePaths = (\n -> "./Examples/" ++ n ++ ".class") . cname <$> tast
+  zipWithM_ encodeClassFile filePaths classFiles
+  where toSnd :: (a -> b) -> a -> (a, b)
+        toSnd f x = (x, f x)
 
 main :: IO ()
-main = do
-  let tastClasses = classes
-  let cps = (map (\x -> (x, buildConstantPool x)) tastClasses)
-  let cf = zipWith (\a b -> ("./javaOut/" ++ (a) ++ ".class", b)) (map cname tastClasses) $ codeGen cps
-
-  let f = (\(fp, cf) -> encodeClassFile (fp) (traceShowId cf))
-  mapM_ f cf
+main = compile
