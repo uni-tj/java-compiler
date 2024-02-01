2ca4ad2036437c3304a053fba2d54836ae50380f
weiserhase <j.keller.wol@gmail.com>
2024-02-01 16:03:18 +0100

    Make ConstantPool Generation More Debugging

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,21 @@
 module Main (main) where
 
-import           ByteCodeGen.ByteCodeGen
-import           ConstantPool.ConstantPool (buildConstantPool)
-import           Control.Monad             (when, zipWithM_)
-import           Debug.Trace               (traceShowId)
-import           Jvm.BinaryClass           (encodeClassFile)
-import           Parser.IndexParser        (parser)
-import           Scanner.Lexer             (scanner)
-import           SemanticCheck.Typecheck   (typecheck)
-import           Types.TAST                (Class (cname))
+import ByteCodeGen.ByteCodeGen
+import ConstantPool.ConstantPool (wrapConstantPool)
+import Control.Monad (when, zipWithM_)
+import Debug.Trace (traceShowId)
+import Jvm.BinaryClass (encodeClassFile)
+import Parser.IndexParser (parser)
+import Scanner.Lexer (scanner)
+import SemanticCheck.Typecheck (typecheck)
+import Types.TAST (Class (cname))
 
 examplePath = "Examples/Main.java"
+
 debug = True
 
 fromRight' :: Either String b -> b
-fromRight' (Left err)   = error err
+fromRight' (Left err) = error err
 fromRight' (Right succ) = succ
 
 compile :: IO ()
@@ -36,7 +37,7 @@ compile = do
   when debug $ do
     print "Typecheck output:"
     print tast
-  let withPools = toSnd buildConstantPool <$> tast
+  let withPools = toSnd wrapConstantPool <$> tast
   when debug $ do
     print "Constant pool output:"
     print "Currently not available"
@@ -47,8 +48,9 @@ compile = do
 
   let filePaths = (\n -> "./Examples/" ++ n ++ ".class") . cname <$> tast
   zipWithM_ encodeClassFile filePaths classFiles
-  where toSnd :: (a -> b) -> a -> (a, b)
-        toSnd f x = (x, f x)
+  where
+    toSnd :: (a -> b) -> a -> (a, b)
+    toSnd f x = (x, f x)
 
 main :: IO ()
 main = compile
