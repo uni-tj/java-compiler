9647953c0dc94ae0b803f993d2ff3da91c5e6be7
Timo Schmidberger <schmidi.timo@gmail.com>
2024-02-01 16:18:31 +0100

    Change exampel

--- Examples/Main.java
+++ Examples/Main.java
@@ -1,5 +1,49 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println(123);
+  Main() {
+    return;
   }
+
+  static int add(int a, int b) {
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    B b = new B();
+
+    System.out.println(add(10,10));
+    System.out.println(B.mul(10, 5));
+    System.out.println(b.mul(10, 5));
+    System.out.println(b.add(10,10));
+
+    return;
+  }
+}
+
+class A {
+  int i;
+
+  A() {
+    this.i = 7;
+    return;
+  }
+
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+}
+
+class B extends A {
+  B() {
+    return;
+  }
+
+  public int add(int a, int b) {
+    return a + b + a + b;
+  }
+
+  public static int mul(int a, int b) {
+    return a * b;
+  }
+
 }
