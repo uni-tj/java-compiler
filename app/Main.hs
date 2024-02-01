cf5ab1f26f7f6bfb50077f6bb6adf76a4b7296a3
p-98 <34243013+p-98@users.noreply.github.com>
2024-02-01 15:50:44 +0100

    main add debug

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main (main) where
 
 import           ByteCodeGen.ByteCodeGen
 import           ConstantPool.ConstantPool (buildConstantPool)
-import           Control.Monad             (zipWithM_)
+import           Control.Monad             (when, zipWithM_)
 import           Debug.Trace               (traceShowId)
 import           Jvm.BinaryClass           (encodeClassFile)
 import           Parser.IndexParser        (parser)
@@ -11,6 +11,7 @@ import           SemanticCheck.Typecheck   (typecheck)
 import           Types.TAST                (Class (cname))
 
 examplePath = "Examples/Main.java"
+debug = True
 
 fromRight' :: Either String b -> b
 fromRight' (Left err)   = error err
@@ -24,10 +25,25 @@ compile = do
   file <- readFile filePath
 
   let lexems = scanner file
+  when debug $ do
+    print "Lexer output:"
+    print lexems
   let ast = fromRight' $ parser lexems
+  when debug $ do
+    print "Parser output:"
+    print ast
   let tast = typecheck ast
+  when debug $ do
+    print "Typecheck output:"
+    print tast
   let withPools = toSnd buildConstantPool <$> tast
+  when debug $ do
+    print "Constant pool output:"
+    print "Currently not available"
   let classFiles = codeGen withPools
+  when debug $ do
+    print "Codegen output:"
+    print classFiles
 
   let filePaths = (\n -> "./Examples/" ++ n ++ ".class") . cname <$> tast
   zipWithM_ encodeClassFile filePaths classFiles
